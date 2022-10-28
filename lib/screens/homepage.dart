20df790741d06c576b6ae1480fcae6241873169d
Emon526 <emonats526@gmail.com>
2022-10-29 00:35:59 +0600

    navigation fixed

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -37,152 +37,22 @@ class _HomePageState extends State<HomePage> {
     return [
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.lock_outline),
-        title: ("Vault"),
+        title: "Vault",
         activeColorPrimary: Theme.of(context).primaryColor,
         inactiveColorPrimary: Colors.grey.shade600,
       ),
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.generating_tokens_outlined),
-        title: ("Generator"),
+        title: "Generator",
         activeColorPrimary: Theme.of(context).primaryColor,
         inactiveColorPrimary: Colors.grey.shade600,
       ),
       PersistentBottomNavBarItem(
         icon: const Icon(Icons.delete_outline),
-        title: ("Trash"),
+        title: "Trash",
         activeColorPrimary: Theme.of(context).primaryColor,
         inactiveColorPrimary: Colors.grey.shade600,
       ),
     ];
   }
 }
-
-
-
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
-
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
-//   int currentIndex = 0;
-//   final screens = [
-//     const VaultPage(),
-//     const GeneratorPage(),
-//     const TrashPage(),
-//   ];
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       // body: IndexedStack(
-//       //   index: currentIndex,
-//       //   children: screens,
-//       // ),
-//       body: screens[currentIndex],
-//       bottomNavigationBar: BottomNavigationBar(
-//         currentIndex: currentIndex,
-//         type: BottomNavigationBarType.fixed,
-//         selectedItemColor: Theme.of(context).primaryColor,
-//         onTap: (value) {
-//           // Respond to item press.
-//           setState(() {
-//             currentIndex = value;
-//           });
-//         },
-//         items: const [
-//           BottomNavigationBarItem(
-//             label: 'Vault',
-//             icon: Icon(Icons.lock_outline),
-//           ),
-//           BottomNavigationBarItem(
-//             label: 'Generator',
-//             icon: Icon(Icons.generating_tokens_outlined),
-//           ),
-//           BottomNavigationBarItem(
-//             label: 'Trash',
-//             icon: Icon(Icons.delete_outline),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   // Widget build(BuildContext context) {
-//   //   return CupertinoTabScaffold(
-//   //     tabBar: CupertinoTabBar(items: const [
-//   //       BottomNavigationBarItem(
-//   //         label: 'Vault',
-//   //         icon: Icon(Icons.lock_outline),
-//   //       ),
-//   //       BottomNavigationBarItem(
-//   //         label: 'Generator',
-//   //         icon: Icon(Icons.generating_tokens_outlined),
-//   //       ),
-//   //       BottomNavigationBarItem(
-//   //         label: 'Trash',
-//   //         icon: Icon(Icons.delete_outline),
-//   //       ),
-//   //     ]),
-//   //     tabBuilder: (context, index) {
-//   //       switch (index) {
-//   //         case 0:
-//   //           return CupertinoTabView(
-//   //             builder: (context) {
-//   //               return CupertinoPageScaffold(child: VaultPage());
-//   //             },
-//   //           );
-//   //         case 1:
-//   //           return CupertinoTabView(
-//   //             builder: (context) {
-//   //               return CupertinoPageScaffold(child: GeneratorPage());
-//   //             },
-//   //           );
-//   //         case 2:
-//   //           return CupertinoTabView(
-//   //             builder: (context) {
-//   //               return CupertinoPageScaffold(child: TrashPage());
-//   //             },
-//   //           );
-//   //         default:
-//   //           CupertinoTabView(
-//   //             builder: (context) {
-//   //               return CupertinoPageScaffold(child: VaultPage());
-//   //             },
-//   //           );
-//   //       }
-//   //       return Container();
-//   //     },
-// //       body: IndexedStack(
-// //         index: currentIndex,
-// //         children: screens,
-// //       ),
-// //       bottomNavigationBar: BottomNavigationBar(
-// //         currentIndex: currentIndex,
-// //         type: BottomNavigationBarType.fixed,
-// //         selectedItemColor: Theme.of(context).primaryColor,
-// //         onTap: (value) {
-// //           // Respond to item press.
-// //           setState(() {
-// //             currentIndex = value;
-// //           });
-// //         },
-// //         items: const [
-// //           BottomNavigationBarItem(
-// //             label: 'Vault',
-// //             icon: Icon(Icons.lock_outline),
-// //           ),
-// //           BottomNavigationBarItem(
-// //             label: 'Generator',
-// //             icon: Icon(Icons.generating_tokens_outlined),
-// //           ),
-// //           BottomNavigationBarItem(
-// //             label: 'Trash',
-// //             icon: Icon(Icons.delete_outline),
-// //           ),
-// //         ],
-// //       ),
-// //     );
-// //   }
-// }
