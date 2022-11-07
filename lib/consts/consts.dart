291cb906992049ae44269d3cccac587e46501774
Emon526 <emonats526@gmail.com>
2022-11-07 22:37:59 +0600

    consts added

--- lib/consts/consts.dart
+++ lib/consts/consts.dart
@@ -1,4 +1,15 @@
+// ignore_for_file: constant_identifier_names
+
 class Consts {
   static const THEME_STATUS = "THEME_STATUS";
   static const APP_NAME = "NepPass";
+  static const LOGO = 'assets/logo.png';
+  static const TITLE_1 = 'Welcome to NepPass!';
+  static const TITLE_2 = 'Trusted and Secure';
+  static const SUBTITLE_1 = 'NepPass is the most secure way to '
+      'store and protect your passwords '
+      'from cybercriminals.';
+  static const SUBTITLE_2 = 'NepPass uses a zero-knowledge '
+      'encryption means your data remains '
+      'private even from us.';
 }
