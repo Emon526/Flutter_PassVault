df5d19a4992512ffad772005b66d0c40d80f6ef2
Emon526 <emonats526@gmail.com>
2022-11-07 23:15:03 +0600

    theme applied

--- lib/screens/settings/chagepassword.dart
+++ lib/screens/settings/chagepassword.dart
@@ -13,38 +13,45 @@ class ChangePasswordPage extends StatefulWidget {
   State<ChangePasswordPage> createState() => _ChangePasswordPageState();
 }
 
-final passwordController = TextEditingController();
-final confirmpasswordController = TextEditingController();
-bool isObsecured = true;
-
-_savePassword(String password) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  await prefs.setString('password', password);
-}
-
-final GlobalKey<FormState> _loginformKey = GlobalKey<FormState>();
-
-final passwordValidator = MultiValidator([
-  RequiredValidator(errorText: 'password is required'),
-  MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
-  PatternValidator(r'(?=.*?[#?!@$%^&*-])',
-      errorText: 'passwords must have at least one special character')
-]);
-
-void validate(BuildContext context) async {
-  final FormState form = _loginformKey.currentState!;
-  if (form.validate()) {
-    await _savePassword(confirmpasswordController.text.trim());
-    Navigator.pop(context);
-  } else {
-    const snackbar = SnackBar(
-      content: Text("Form is invalid"),
-    );
-    ScaffoldMessenger.of(context).showSnackBar(snackbar);
-  }
-}
-
 class _ChangePasswordPageState extends State<ChangePasswordPage> {
+  final passwordController = TextEditingController();
+  final confirmpasswordController = TextEditingController();
+  bool isObsecured = true;
+
+  _savePassword(String password) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('password', password);
+  }
+
+  final GlobalKey<FormState> _loginformKey = GlobalKey<FormState>();
+
+  final passwordValidator = MultiValidator([
+    RequiredValidator(errorText: 'password is required'),
+    MinLengthValidator(8, errorText: 'password must be at least 8 digits long'),
+    PatternValidator(r'(?=.*?[#?!@$%^&*-])',
+        errorText: 'passwords must have at least one special character')
+  ]);
+
+  void validate(BuildContext context) async {
+    final FormState form = _loginformKey.currentState!;
+    if (form.validate()) {
+      await _savePassword(confirmpasswordController.text.trim());
+      Navigator.pop(context);
+    } else {
+      const snackbar = SnackBar(
+        content: Text("Form is invalid"),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+    }
+  }
+
+  @override
+  void dispose() {
+    passwordController.dispose();
+    confirmpasswordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -59,77 +66,74 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
         ),
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: size.width * 0.07,
-            ),
-            child: Form(
-              key: _loginformKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: size.height * 0.07,
-                  ),
-                  TextFormField(
-                    obscureText: isObsecured,
-                    controller: passwordController,
-                    keyboardType: TextInputType.visiblePassword,
-                    textInputAction: TextInputAction.next,
-                    validator: passwordValidator,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      // border: const OutlineInputBorder(),
-                      suffix: InkWell(
-                        child: Icon(
-                          isObsecured ? Icons.visibility : Icons.visibility_off,
-                        ),
-                        onTap: () {
-                          setState(() {
-                            isObsecured = !isObsecured;
-                          });
-                        },
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: size.width * 0.07,
+          ),
+          child: Form(
+            key: _loginformKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.height * 0.04,
+                ),
+                TextFormField(
+                  obscureText: isObsecured,
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.next,
+                  validator: passwordValidator,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    // border: const OutlineInputBorder(),
+                    suffix: InkWell(
+                      child: Icon(
+                        isObsecured ? Icons.visibility : Icons.visibility_off,
                       ),
+                      onTap: () {
+                        setState(() {
+                          isObsecured = !isObsecured;
+                        });
+                      },
                     ),
                   ),
-                  SizedBox(
-                    height: size.height * 0.03,
-                  ),
-                  TextFormField(
-                    obscureText: isObsecured,
-                    controller: confirmpasswordController,
-                    keyboardType: TextInputType.visiblePassword,
-                    textInputAction: TextInputAction.done,
-                    validator: (val) => MatchValidator(
-                            errorText: 'passwords do not match')
-                        .validateMatch(val!, passwordController.text.trim()),
-                    decoration: InputDecoration(
-                      labelText: 'Confirm Password',
-                      // border: const OutlineInputBorder(),
-                      suffix: InkWell(
-                        child: Icon(
-                          isObsecured ? Icons.visibility : Icons.visibility_off,
-                        ),
-                        onTap: () {
-                          setState(() {
-                            isObsecured = !isObsecured;
-                          });
-                        },
+                ),
+                SizedBox(
+                  height: size.height * 0.03,
+                ),
+                TextFormField(
+                  obscureText: isObsecured,
+                  controller: confirmpasswordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  textInputAction: TextInputAction.done,
+                  validator: (val) =>
+                      MatchValidator(errorText: 'passwords do not match')
+                          .validateMatch(val!, passwordController.text.trim()),
+                  decoration: InputDecoration(
+                    labelText: 'Confirm Password',
+                    // border: const OutlineInputBorder(),
+                    suffix: InkWell(
+                      child: Icon(
+                        isObsecured ? Icons.visibility : Icons.visibility_off,
                       ),
+                      onTap: () {
+                        setState(() {
+                          isObsecured = !isObsecured;
+                        });
+                      },
                     ),
                   ),
-                  SizedBox(
-                    height: size.height * 0.04,
-                  ),
-                  CustomButton(
-                    ontap: () {
-                      validate(context);
-                    },
-                    buttontext: 'Change Password',
-                  ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: size.height * 0.04,
+                ),
+                CustomButton(
+                  ontap: () {
+                    validate(context);
+                  },
+                  buttontext: 'Change Password',
+                ),
+              ],
             ),
           ),
         ),
