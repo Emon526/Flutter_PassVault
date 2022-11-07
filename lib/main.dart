67d0ed81655f154603e2a453c482ee5d90ded400
Emon526 <emonats526@gmail.com>
2022-11-08 04:46:45 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'consts/consts.dart';
@@ -9,20 +7,14 @@ import 'provider/addpasswordprovider.dart';
 import 'provider/generatedpassswordprovideer.dart';
 import 'provider/themeprovider.dart';
 import 'screens/auth/login.dart';
-import 'screens/homepage.dart';
 import 'screens/onboardingpage.dart';
 import 'services/databaseservice.dart';
 
 int? isviewed;
 void main() async {
-  // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-  //   // statusBarColor: Colors.transparent,
-  //   statusBarIconBrightness: Brightness.light,
-  // ));
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   isviewed = prefs.getInt('onBoard');
-
   runApp(const MyApp());
 }
 
@@ -64,14 +56,12 @@ class _MyAppState extends State<MyApp> {
       ],
       builder: (context, child) {
         context.read<AddPasswordProvider>().fatchdata;
-        //context.read<GeneratedPasswordProvider>().generatePassword;
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           title: Consts.APP_NAME,
           theme: Styles.themeData(
               context.watch<ThemeProvider>().getDarkTheme, context),
-          // home: isviewed != 0 ? const OnBoardingSceen() : const LoginPage(),
-          home: isviewed != 0 ? const OnBoardingSceen() : const HomePage(),
+          home: isviewed != 0 ? const OnBoardingSceen() : const LoginPage(),
         );
       },
     );
